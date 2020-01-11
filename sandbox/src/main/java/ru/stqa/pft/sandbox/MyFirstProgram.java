3c1d0e6aeb9b2bc66041d087016c8b1e645d2c5c
lumen4000 <sobolevalex95@yandex.ru>
2020-01-12 01:05:01 +0300

    Lesson 1.13 - Functions

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,14 +1,33 @@
 package ru.stqa.pft.sandbox;
 
+import javax.print.attribute.SetOfIntegerSyntax;
+import javax.sound.midi.Soundbank;
+import java.sql.SQLOutput;
+
 public class MyFirstProgram {
 
   public static void main(String[] args) {
-    System.out.println("Hello,world");
+    hello("world");
+    hello("user");
+    hello("Alex");
 
-    int l = 6;
-    int s = l * l ;
-    System.out.println( "Площадь квадрата со стороной "+ l + "="  + s);
+    double l = 5 ;
+    System.out.println("Площадь квадрата со стороной " + l + "=" + area(l));
+
+    double a = 4;
+    double b = 6;
+    System.out.println("Площадь прямоугольника со сторонами " + a + "и" + b + "=" + area(a,b));
   }
 
+    public static  void hello(String somebody) {
+    System.out.println("Hello, " + somebody + "!");
+    }
 
-}	
+    public  static double area(double l ){
+      return  l * l;
+    }
+   public  static  double area(double a , double b ) {
+    return  a * b;
+   }
+}
+
