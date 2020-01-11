286133ef727adbdb64d5167c81a6809b0216d079
lumen4000 <sobolevalex95@yandex.ru>
2020-01-12 01:35:06 +0300

    Lesson 1.14 - Class and objects

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -11,23 +11,23 @@ public class MyFirstProgram {
     hello("user");
     hello("Alex");
 
-    double l = 5 ;
-    System.out.println("Площадь квадрата со стороной " + l + "=" + area(l));
 
-    double a = 4;
-    double b = 6;
-    System.out.println("Площадь прямоугольника со сторонами " + a + "и" + b + "=" + area(a,b));
+    Square s = new Square(5);
+    System.out.println("Площадь квадрата со стороной " + s.l + "=" + area(s));
+
+    Rectangle r = new Rectangle(4,6);
+    System.out.println("Площадь прямоугольника со сторонами " + r.a + "и" + r.a + "=" + area(r));
   }
 
     public static  void hello(String somebody) {
     System.out.println("Hello, " + somebody + "!");
     }
 
-    public  static double area(double l ){
-      return  l * l;
+    public  static double area(Square s){
+      return  s.l * s.l;
     }
-   public  static  double area(double a , double b ) {
-    return  a * b;
+   public  static  double area(Rectangle r ) {
+    return  r.a * r.b;
    }
 }
 
